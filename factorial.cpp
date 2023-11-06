5153bac41a36168ec4b174b66fb14d0803d05288
bdbrink <brendan.brink@outlook.com>
2023-11-06 16:49:33 -0600

    adding factorial script

---
+++ factorial.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+// Function to calculate factorial using recursion
+unsigned long long factorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
+int main() {
+    int number;
+    
+    std::cout << "Enter a positive integer: ";
+    std::cin >> number;
+
+    if (number < 0) {
+        std::cout << "Factorial is not defined for negative numbers." << std::endl;
+    } else {
+        unsigned long long result = factorial(number);
+        std::cout << "Factorial of " << number << " is: " << result << std::endl;
+    }
+
+    return 0;
+}
